e29d1b9469a5a624fbf1f4ba711f863e04b09cf4
iamjkh <iamjkh@gmail.com>
2024-10-08 22:59:10 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241008/3 또는 5의 배수/multiples-of-3-or-5.cpp
+++ 241008/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -6,12 +6,15 @@ int main() {
 
     cin >> a;
 
-    for(int i=0; i<2; i++)
-    {
-        if (a % 3 == 0 || a%5 == 0)
-            cout << "YES" << "\n";
-        else
-            cout << "NO" << "\n";
-    }
+    if (a % 3 == 0)
+        cout << "YES" << "\n";
+    else
+        cout << "NO" << "\n";
+
+    if (a % 5 == 0)
+        cout << "YES" << "\n";
+    else
+        cout << "NO" << "\n";
+
     return 0;
 }
