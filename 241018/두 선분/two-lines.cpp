df349e74c03b88a459004826a4ebf0a0649d2959
iamjkh <iamjkh@gmail.com>
2024-10-18 12:43:17 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241018/두 선분/two-lines.cpp
+++ 241018/두 선분/two-lines.cpp
@@ -6,7 +6,7 @@ int main() {
 
     cin >> x1 >> x2 >> x3 >> x4;
 
-    if( x2 >=x3 && x4 > x3)
+    if( x2 >=x3 && x4 > x3 && !(x3 < x1 && x4 < x1))
         cout << "intersecting";
     else
         cout << "nonintersecting";
