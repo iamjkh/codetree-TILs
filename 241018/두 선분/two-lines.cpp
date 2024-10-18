face524b024c0215ee30c2c99f153a0639901ca5
iamjkh <iamjkh@gmail.com>
2024-10-18 12:41:24 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241018/두 선분/two-lines.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1, x2, x3, x4;
+
+    cin >> x1 >> x2 >> x3 >> x4;
+
+    if(x3 >= x1 && x2 >=x3 && x4 > x3)
+        cout << "intersecting";
+    else
+        cout << "nonintersectiing";
+
+    return 0;
+}
